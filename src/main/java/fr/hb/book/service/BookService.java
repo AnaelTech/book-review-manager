849f1c6663e37ea56b6051aaf6f0c00ae54fd2b0
Anael Payet <anaelpayetpro@gmail.com>
2025-06-11 15:11:20 +0200

    ✨ feat : Add methodes and javacdoc

--- src/main/java/fr/hb/book/service/BookService.java
+++ src/main/java/fr/hb/book/service/BookService.java
@@ -2,4 +2,41 @@ package fr.hb.book.service;
 
 public class BookService {
 
+  /**
+   * Displays the list of books sorted by average rating.
+   * 
+   * @param books the list of books
+   */
+  public void displayBooksSortedByAverageRating() {
+
+  }
+
+  /**
+   * Find the best books by genre.
+   * 
+   * @param books the list of books
+   */
+  public void findBestBooksByGenre() {
+
+  }
+
+  /**
+   * Filters the books published before a certain year and displays their reviews.
+   * 
+   * @param books the list of books
+   */
+  public void filterBooksPublishedBeforeYearAndShowReviews() {
+
+  }
+
+  /**
+   * Groups the books by genre and displays the count and average rating for each
+   * group.
+   * 
+   * @param books the list of books
+   */
+  public void groupBooksByGenreAndShowCountAndAverageRating() {
+
+  }
+
 }
--- src/main/java/fr/hb/book/service/ReviewService.java
+++ src/main/java/fr/hb/book/service/ReviewService.java
@@ -2,4 +2,14 @@ package fr.hb.book.service;
 
 public class ReviewService {
 
+  /**
+   * Displays the list of reviews sorted by average rating.
+   * 
+   * @param reviews the list of reviews
+   * @param author  name
+   */
+  public void displayReviewsByAuthorSortedByDateDesc() {
+
+  }
+
 }
